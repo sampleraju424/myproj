eae6ad0d19bde900b11fb44e1e11f6b7cead11f6
Raj <chennaboinalingaraju424@gmail.com>
2020-02-22 13:27:28 +0530

    Employe class add the setter and getters added

--- src/main/java/raju/nit/model/Employe.java
+++ src/main/java/raju/nit/model/Employe.java
@@ -1,11 +1,40 @@
 package raju.nit.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Entity
+@Table(name = "emp")
 public class Employe {
 	
 	
 	private Integer id;
 	private String name;
 	private String address;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Employe [id=" + id + ", name=" + name + ", address=" + address + "]";
+	}
 	
-
+	
 }
